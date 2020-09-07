38c9ae40392ec6132c6d7163bc665cd347310df0
Xiaomi <inyeroos@gmail.com>
2020-09-07 10:35:20 +0300

    yo

---
+++ src/com/nyeroos/firstlesson/ex4.java
@@ -0,0 +1,10 @@
+package com.nyeroos.firstlesson;
+
+public class ex4 {
+    public static void main(String[] args) {
+        for (int i=0; i<args.length; i++){
+            System.out.println(args[i]);
+        }
+
+    }
+}
---
+++ src/com/nyeroos/firstlesson/ex6.java
@@ -0,0 +1,5 @@
+package com.nyeroos.firstlesson;
+
+public class ex6 {
+
+}
