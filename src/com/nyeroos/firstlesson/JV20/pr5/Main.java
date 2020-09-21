714037f1dbc405d7464e5f4f060e04ac900bd358
Xiaomi <inyeroos@gmail.com>
2020-09-21 11:13:12 +0300

    fifth und sixth ist fertig (21.09)

---
+++ src/com/nyeroos/firstlesson/JV20/pr5/Main.java
@@ -0,0 +1,16 @@
+package com.nyeroos.firstlesson.JV20.pr5;
+
+public class Main {
+    public static void main(String[] args) {
+        MovableCircle circle = new MovableCircle(1,10,2,7,5);
+        System.out.println(circle);
+        circle.moveLeft();
+        System.out.println(circle);
+        circle.moveDown();
+        System.out.println(circle);
+        circle.moveRight();
+        System.out.println(circle);
+        circle.moveUp();
+        System.out.println(circle);
+    }
+}
---
+++ src/com/nyeroos/firstlesson/JV20/pr6/MovablePoint.java
@@ -0,0 +1,40 @@
+package com.nyeroos.firstlesson.JV20.pr6;
+
+public class MovablePoint implements Movable{
+    protected int x;
+    protected int y;
+    protected int xSpeed;
+    protected int ySpeed;
+
+    public MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "x=" + x +
+                ", y=" + y +
+                ", xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+
+    public void moveUp(){
+        this.y=+ySpeed;
+    }
+
+    public void moveDown(){
+        this.y=-ySpeed;
+    }
+    public void moveLeft(){
+        this.x=-xSpeed;
+    }
+
+    public void moveRight(){
+        this.x=+xSpeed;
+    }
+}
